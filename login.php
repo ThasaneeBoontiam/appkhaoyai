d27db932b794b0a7805da5ce03df35ee292f1d9d
Wetpisit <6306021422194@fitm.kmutnb.ac.th>
2022-06-27 15:13:49 +0700

    test

--- login.php
+++ login.php
@@ -96,6 +96,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
+    <!-- <link rel="icon" type="image/x-icon" href="/img/Khaoyai-Logo.ico"> -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
